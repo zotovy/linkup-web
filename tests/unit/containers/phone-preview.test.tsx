1fc1914561fa8b81f277e93f70a48163d43f8f9c
Yaroslav Zotov <35657233+zotovY@users.noreply.github.com>
2021-04-09 22:04:17 +0500

    create unit tests for PhonePreview container

---
+++ tests/unit/containers/phone-preview.test.tsx
@@ -0,0 +1,20 @@
+/// <reference types="@types/jest" />
+import { render } from "../../test-utils";
+import '@testing-library/jest-dom';
+import 'jest-styled-components';
+
+import PhonePreview, { Props } from "@/containers/phone-preview";
+import { cleanup } from "@testing-library/react";
+import { user } from "../../data";
+
+describe("Testing PhonePreview component", () => {
+
+    afterEach(cleanup);
+
+    const props: Props = user;
+
+    test("should renders correctly", () => {
+        const { asFragment } = render(<PhonePreview { ...props } />);
+        expect(asFragment()).toMatchSnapshot();
+    });
+});
